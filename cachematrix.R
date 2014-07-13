96d60eb85912432626b9979e8ac9e3459bc8de9c
Jack Kwok <jkwok@trulia.com>
2014-07-13 12:55:20 -0700

    tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,25 @@
+## Peer-graded Programming Assignment 2 of R Programming Coursera class
 ## Matrix inversion is usually a costly computation.  These functions use caching to improve performance. 
 ## 
-
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-  invMatrix <- matrix()
+  x <- x
+  invMatrix <- NULL
   set <- function(y) {
     x <<- y
     invMatrix <<- NULL
   }
-  get <- function() x
-  setInverse <- function(inv) invMatrix <<- inv 
-  getInverse <- function() invMatrix
+  get <- function() {
+    x
+  }
+  setInverse <- function(inv) {
+    invMatrix <<- inv 
+  }
+  getInverse <- function() {
+    invMatrix
+  }
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
@@ -24,11 +31,11 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   invMatrix <- x$getInverse()
   
   if(!is.null(invMatrix)) {
-    messag("getting cached data")
+    message("getting cached data")
     return (invMatrix)
   }
   data <- x$get()
@@ -36,3 +43,14 @@ cacheSolve <- function(x, ...) {
   x$setInverse(invMatrix)
   invMatrix
 }
+
+## Tests
+
+seq1 <- c(1,0,1,2,4,0,3,5,6)
+mat1 <- matrix(seq1, nrow=3, ncol=3)
+m1 <- makeCacheMatrix(mat1)
+cacheSolve(m1)
+
+m <- makeCacheMatrix(matrix(1:4, nrow=2))
+
+
